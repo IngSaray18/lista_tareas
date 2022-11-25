fbddfc6271bd23896dd0717639d72d4a8390f89c
IngSaray18 <pedrosaray18@hotmail.com>
2022-11-24 19:51:54 -0600

    comit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return ( 
+        <header className='header'>
+            <h1 className='header-title'>lista de tareas</h1>
+            <button className='header-button'>no mostrar tareas compledatadas</button>
+        </header>
+     );
+}
+ 
+export default Header;
