e4163b07ae002bfb47e05e1fec2f05bb1fc13280
IngSaray18 <pedrosaray18@hotmail.com>
2022-11-24 22:34:21 -0600

    dos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/header";
+import Tareas from './components/tareas';
 
 function App() {
   return (
 <div className='contenedor'>
     <Header/>
+    <Tareas/>
 </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye ,faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return ( 
         <header className='header'>
             <h1 className='header-title'>lista de tareas</h1>
-            <button className='header-button'>no mostrar tareas compledatadas</button>
+            <button className='header-button'>no mostrar tareas compledatadas
+            <FontAwesomeIcon className='header-iconButton' icon={faEyeSlash}  />
+            </button>
         </header>
      );
 }
